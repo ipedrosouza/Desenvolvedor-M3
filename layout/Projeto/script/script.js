b8b6aa5cdc8dfd0a49dc1a892b30e7defb4fb0ad
Pedro Lucas Souza <plucassouza13@gmail.com>
2020-04-21 18:05:17 -0300

    Projeto final

--- layout/Projeto/script/script.js
+++ layout/Projeto/script/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     exibeProdutos(produtos)
     var carrinho =[];
-    
-    $('.buy').on('click', function() {
+    var produtosExibidos = produtos;
+
+    $('body').on('click', '.buy', function() {
         var id = $(this).data('id');
+        // alert(id);
         var qtdProdutos = produtos.length;
         
         for(i=0; i < qtdProdutos; i++){
@@ -12,72 +14,73 @@ $(document).ready(function () {
             }
         }
         $('.count')
-        .text(carrinho.length);
+            .text(carrinho.length);
         var cartCountValue = 0;
         var cartCount = $('.cart .count');
         $(cartCount).text(cartCountValue);
-
     })
-    $(function () {
-        $(".box_produtcs").slice(0, 3).show();
-        $(".footer_btn").on('click', function (e) {
-            e.preventDefault();
-            $(".box_produtcs:hidden").slice(0, 3).slideDown();
-            if($(".box_produtcs:hidden").length == 0) {
-                $(".replace").text('NO CONTENT');
-            }
-            $('html,body').animate({
-                scrollTop: $(this).offset().top
-            }, 1500);
-        });
-        
-    });
-    
 
     $('.custom-select>select').change(function(){
         var ordenacao=$(this).val();
-        var estoqueOrdenado=ordem(produtos,ordenacao)
-        console.log(estoqueOrdenado)
-    })
-
-    $('#filter_form').submit(function(e){
-            var estoqueFilter = produtos;
-            
-            e.preventDefault();
-            var cores = [];
-            $('input[name=cor]:checked').each(function(){
-                cores.push($(this).val());
-                
-            })
-           
-            var tamanhos = [];
-            $('input[name=tamanho]:checked').each(function(){
-                tamanhos.push($(this).val());
-                
-            })
-            var preco = $('input[name=preco]').val();
-
-            console.log(tamanhos);
-            if(tamanhos.length>0){
-                var estoqueFilter = filtraTamanho(tamanhos, produtos);
-            }
-
-            if(cores.length>0){
-                var estoqueFilter = filtraCor(cores, produtos);
-            }
-
-            if(valor.length>0){
-                 var estoqueFilter = filtraPreco(valor, produtos);
-            }    
-            console.log(estoqueFilter);
-            
-
-            
-        })
+        var estoqueOrdenado=ordem(produtosExibidos,ordenacao);
+        exibeProdutos(estoqueOrdenado);
         
     })
 
+    $('.content_select>div').click(function() {
+        var ordenacao = $(this).data('order');
+        var estoqueOrdenado = ordem(produtosExibidos,ordenacao);
+        exibeProdutos(estoqueOrdenado);
+        $('.order_sidebar').removeClass('order_open');
+    })
 
+    $('#filter_form').submit(function(e){
+        var estoqueFilter = produtos;
+        e.preventDefault();
+        var cores = [];
+        $('input[name=cor]:checked').each(function(){
+            cores.push($(this).val());
+            
+        })
+       
+        var tamanhos = [];
+        $('input[name=tamanho]:checked').each(function(){
+            tamanhos.push($(this).val());
+            
+        })
+        var preco = [];
+
+        $('input[name=preco]:checked').each(function(){
+            preco.push($(this).val());
+        })
+
+        /* console.log(tamanhos);*/
+        if(preco.length>0){
+             var estoqueFilter = filtraPreco(preco, estoqueFilter);
+        }  
+
+        if(tamanhos.length>0){
+            var estoqueFilter = filtraTamanho(tamanhos, estoqueFilter);
+        }
+
+        if(cores.length>0){
+            var estoqueFilter = filtraCor(cores, estoqueFilter);
+        }
+
+        if(estoqueFilter.length > 0){
+            exibeProdutos(estoqueFilter);
+        }else{
+            $('.box_products').empty();
+            $('.box_products').append('<div class="NoContentMsg"><h3> Não encontramos produtos com este filtro.</h3></div>');
+            $('.footer_btn').empty();
+        }
+        $('.filter_sidebar').removeClass('filter_open');
+        $('body').css('overflow-y','scroll');
+        produtosExibidos = estoqueFilter;
+        
+    })
+   
+        
     function filtraTamanho(tamanhos, produtos){
         var estoqueFiltrado = [];
         var tamanhoEstoque = produtos.length;
@@ -111,15 +114,30 @@ $(document).ready(function () {
         }
         return estoqueFiltrado;
     }
+
     function filtraPreco(valor, produtos){
         var estoqueFiltrado = [];
+
         var tamanhoEstoque = produtos.length;
+        // alert(tamanhoEstoque);
         var qtdValores = valor.length;
+
         for(i=0; i < tamanhoEstoque; i++){ 
+
             for(j=0; j < qtdValores; j++){
-                if(produtos[i].valor == valores[j]){
-                    estoqueFiltrado.push(produtos[i]);
+                
+                if(valor[j]!=501){
+                    var range = valor[j].split("-");
+                    if(range[0] <= produtos[i].valor && range[1] >= produtos[i].valor){
+                        estoqueFiltrado.push(produtos[i]);
+                    }
+                }else{
+                    if(produtos[i].valor >= valor[j]){
+                        estoqueFiltrado.push(produtos[i]);
+                    }
                 }
+
+                
             }
         }
         
@@ -173,75 +191,89 @@ $(document).ready(function () {
     
     
     function exibeProdutos (produtos){
+        $('.box_products').empty();
+        $('.footer_btn').empty();
+        
         var qtdProdutos=produtos.length
         for(i=0;i<qtdProdutos;i++){
             var valorFinal=produtos[i].valor/produtos[i].parcelas;
             $('.box_products').append(`<div class=\"single_product\"><img src=\"../imagens/${produtos[i].img}.png\" alt=\"\"><div class=\"info_product\"><h4>${produtos[i].nome}</h4><p><span><strong> R$ ${(produtos[i].valor).toFixed(2)}</strong></span></p><p> Até ${produtos[i].parcelas}x de R$${(valorFinal).toFixed(2)}</p><div data-id=\"${produtos[i].id}\" class=\"buy\"><a >Comprar</a></div></div></div>`)
         }
-        
+        if(qtdProdutos>9){
+            $('.footer_btn').append('<div id="loadMore"><a><span class="replace">CARREGAR MAIS</span></a></div>')              
+        }    
+        $(".single_product").slice(0, 9).show();     
     }
     
+    $(".footer_btn").on('click', function (e) {
+        e.preventDefault();
+        $(".single_product:hidden").slice(0, 3).slideDown();
+        if($(".single_product:hidden").length == 0) {
+            $(".replace").text('Fim dos produtos');
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
 
-$(".sidebar-dropdown > a").click(function() {
-    $(".sidebar-submenu").slideUp(200);
-    if (
-      $(this)
-        .parent()
-        .hasClass("active")
-    ) {
-      $(".sidebar-dropdown").removeClass("active");
-      $(this)
-        .parent()
-        .removeClass("active");
-    } else {
-      $(".sidebar-dropdown").removeClass("active");
-      $(this)
-        .next(".sidebar-submenu")
-        .slideDown(200);
-      $(this)
-        .parent()
-        .addClass("active");
-    }
-  });
-  
-  $("#close-sidebar").click(function() {
-    $(".page-wrapper").removeClass("toggled");
-});
-  $("#show-sidebar").click(function() {
-    $(".page-wrapper").addClass("toggled");
-});
- $('select').change(function(evt) {
-    var theElement = $(this);
-    if (!theElement.data("selected")) { 
-      theElement.children("option")[0].remove(); 
-      theElement.data("selected", true); 
-    }
-});
+    $(".sidebar-dropdown > a").click(function() {
+        $(".sidebar-submenu").slideUp(200);
+        if (
+          $(this)
+            .parent()
+            .hasClass("active")
+        ) {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .parent()
+            .removeClass("active");
+        } else {
+          $(".sidebar-dropdown").removeClass("active");
+          $(this)
+            .next(".sidebar-submenu")
+            .slideDown(200);
+          $(this)
+            .parent()
+            .addClass("active");
+        }
+      });
+      
+      $("#close-sidebar").click(function() {
+        $(".page-wrapper").removeClass("toggled");
+    });
 
- $('.botao-filtrar').on('click', function(){
-    $('.filter_sidebar').addClass('filter_open');
-    $('body').css('overflow-y','hidden');
- })
+    $("#show-sidebar").click(function() {
+        $(".page-wrapper").addClass("toggled");
+    });
+    $('select').change(function(evt) {
+        var theElement = $(this);
+        if (!theElement.data("selected")) { 
+          theElement.children("option")[0].remove(); 
+          theElement.data("selected", true); 
+        }
+    });
 
- $('.close_filter_btn').on('click', function(){
+     $('.botao-filtrar').on('click', function(){
+        $('.filter_sidebar').addClass('filter_open');
+        $('body').css('overflow-y','hidden');
+     })
 
-    $('.filter_sidebar').removeClass('filter_open');
-    $('body').css('overflow-y','scroll');
-    
+    $('.close_filter_btn').on('click', function(){
+        $('.filter_sidebar').removeClass('filter_open');
+        $('body').css('overflow-y','scroll');
+    })
 
- $('.botao-ordenar').on('click', function(){
-    $('.order_sidebar').addClass('order_open');
-    $('body').css('overflow-y','hidden');
-    
+    $('.botao-ordenar').on('click', function(){
+        $('.order_sidebar').addClass('order_open');
+        $('body').css('overflow-y','hidden');
     })
     
     $('.close_order_btn').on('click', function(){
-    $('.order_sidebar').removeClass('order_open');
-    $('body').css('overflow-y','scroll');
+        $('.order_sidebar').removeClass('order_open');
+        $('body').css('overflow-y','scroll');
     })
-    
-   
-     
-    
-   
+    $(".clear-mobile-btn").on('click',function (e) {
+        $('#filter_form').find(':input').not(':button, :submit, :reset, :hidden, :checkbox,:radio').val('');
+        $('#filter_form').find(':checkbox, :radio').prop('checked', false);
+    })
 })
